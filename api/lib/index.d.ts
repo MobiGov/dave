9e449e9fe8fba52179b863c7dfebeda28f03e21b
David Daniel <dadanie@microsoft.com>
2020-07-09 19:45:20 -0400

    modifying build pipeline and removing lib build folders

--- api/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { HttpRequest, Context } from "@azure/functions";
-import { Response } from "./types/az-function";
-export default function (context: Context, req: HttpRequest): Promise<Response>;
