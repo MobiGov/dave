13957cb3a16dca51c13ce6788407d84793ee34ca
David Daniel <dadanie@microsoft.com>
2020-07-13 20:29:27 -0400

    changing typescript version output

--- api/main/index.ts
+++ api/main/index.ts
@@ -1,9 +1,5 @@
 import { AzureFunction, HttpRequest, Context } from "@azure/functions";
 import { Response } from "./types/az-function";
-import "regenerator-runtime/runtime";
-import "core-js/es/array";
-import "core-js/es/promise";
-import "core-js/features/promise";
 
 const httpTrigger: AzureFunction = async function(context: Context, req: HttpRequest): Promise<Response> {
     context.log('TypeScript HTTP trigger function processed a request.');
